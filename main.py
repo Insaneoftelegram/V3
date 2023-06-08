e5750fcf7ead0d32c6664eb99029cee271ec27f3
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-06-09 01:25:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ async def owner_cmd(client, message):
 async def song(client, message):
     cap = "@insanex3"
     url = message.text
-    rkp = await message.reply("Processing")
+    rkp = await message.reply("You not get music in dm.. join @SONGS_WORLDS_OFFICIAL and you will search the song.....")
     search = SearchVideos(url, offset=1, mode="json", max_results=1)
     test = search.result()
     p = json.loads(test)
